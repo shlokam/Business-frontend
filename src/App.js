fac84dde34787db552a25f8664348e34be855e19
Shloka Mahesheka <shloka.mahesheka012@gmail.com>
2022-12-20 16:38:39 +0530

    changes.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+// import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { HashRouter as Router} from "react-router-dom";
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Teams from './components/Teams';
